6259bc2ae1edef38af44575196b37f341c2805aa
KimMJ <ss59s@nate.com>
2020-01-09 02:17:07 +0900

    update blog with script 2020-01-09 02:17:07

--- js/learn.js
+++ js/learn.js
@@ -179,7 +179,7 @@ jQuery(document).ready(function() {
         var code = $(this),
             text = code.text();
 
-        if (text.length > 5) {
+        if ($(this).closest("div").hasClass('highlight')) {
             if (!clipInit) {
                 var text, clip = new ClipboardJS('.copy-to-clipboard', {
                     text: function(trigger) {
